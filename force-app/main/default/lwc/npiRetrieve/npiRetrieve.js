79b16dcf0259b8ddc81c070021edff6649c2eb3f
Ray Dehler <ray@remio.com>
2024-07-21 19:46:35 -0700

    Functional quick action

--- force-app/main/default/lwc/npiRetrieve/npiRetrieve.js
+++ force-app/main/default/lwc/npiRetrieve/npiRetrieve.js
@@ -1,8 +1,32 @@
 import { LightningElement, api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { CloseActionScreenEvent } from 'lightning/actions';
+import retrieveAndUpdateNPI from '@salesforce/apex/NPIRetrieveController.retrieveAndUpdateNPI';
+import { updateRecord } from 'lightning/uiRecordApi';
 
 export default class NpiRetrieve extends LightningElement {
-    @api recordId;
-    @api invoke() {
-        console.log("Hi, I'm an action.",this.recordId);
+    loading = true;
+    error;
+    _recordId;
+
+    @api set recordId(value) {
+        this._recordId = value;
     }
+    
+    get recordId() {
+        return this._recordId;
+    }
+    
+    @api async invoke() {
+        try {
+            await retrieveAndUpdateNPI({ recordId: this.recordId });
+            console.log('retrieved...');
+            updateRecord({ fields: { Id: this.recordId } });
+            console.log('refreshed.');
+            this.error = undefined;
+        } catch (error) {
+            this.error = error;
+        }
+    }
+
 }
